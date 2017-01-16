1a7f3ca81dbc492e5f40ae57b0ce66be7aa03bd9
Eric Guittiere <00095114@uwa.edu.au>
2017-01-16 15:35:42 +0800

    smaller UUID module

--- app/templates/js/modules/UUID.js
+++ app/templates/js/modules/UUID.js
@@ -1,5 +1,5 @@
 export default function() {
-  return ([ 10000000 ] + -1000 + -4000 + -8000 + -100000000000).replace(
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(
     /[018]/g,
     a => (a ^ Math.random() * 16 >> a / 4).toString(16)
   );

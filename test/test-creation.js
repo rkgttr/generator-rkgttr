541a236719569564db046f2603e77ccc4e345408
Eric Guittiere <00095114@uwa.edu.au>
2017-01-20 14:22:59 +0800

    updated to succeed tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -19,9 +19,7 @@ describe('rkgttr generator', function () {
 
   it('creates expected files', function (done) {
     var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+
     ];
 
     helpers.mockPrompt(this.app, {
